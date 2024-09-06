17db62ee4451dceb42f1cdb4c2fb3e931250a068
Dkrisztan <krisztidancs@gmail.com>
2024-09-06 11:10:36 +0200

    dotenv and swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { BACKEND_PORT, BACKEND_URL } from '../utils/configurations';
-import { Logger } from '@nestjs/common';
+import { BACKEND_PORT, BACKEND_URL } from './utils/configurations';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Dine and Dash')
+    .setDescription('Api description for Dine and Dash full stack food delivery application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(BACKEND_PORT || 3001);
 }
 bootstrap().then(() => Logger.log(`Backend server running on ${BACKEND_URL}:${BACKEND_PORT || 3001}`));
