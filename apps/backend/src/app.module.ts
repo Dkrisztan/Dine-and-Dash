4328e8f5333799bfa2ab2d5270d5bf43f4cfc8cd
Dkrisztan <krisztidancs@gmail.com>
2024-09-06 21:52:55 +0200

    creating resources

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { FoodModule } from './food/food.module';
+import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
+import { RestaurantModule } from './restaurant/restaurant.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, FoodModule, CartModule, OrderModule, RestaurantModule],
   controllers: [AppController],
   providers: [AppService],
 })
