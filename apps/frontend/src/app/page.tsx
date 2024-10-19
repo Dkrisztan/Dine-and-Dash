e41030a0a878901c7e1c1b200658ba6502b269b9
Dancs Krisztian <krisztian.d@taboola.com>
2024-10-20 00:13:58 +0200

    deploy

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -8,6 +8,7 @@ export default async function HomePage() {
   const { data: restaurants } = await restaurantApi.restaurantControllerFindAll();
   return (
     <div className='flex flex-col items-center'>
+      <h1 className='text-4xl font-bold mt-10'>Restaurants</h1>
       <Test />
       <div className='px-8 mt-10 grid md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-1 gap-7'>
         {restaurants.map((item, index) => {
