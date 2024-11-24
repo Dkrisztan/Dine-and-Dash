b95a1676366e176797b47b5e25fb41eb7261d5bd
Dkrisztan <krisztidancs@gmail.com>
2024-11-24 16:01:14 +0100

    stuff

--- apps/frontend/src/app/admin/users/[id]/page.tsx
+++ apps/frontend/src/app/admin/users/[id]/page.tsx
@@ -4,17 +4,14 @@ import { notFound } from 'next/navigation';
 
 import AdminProfileInfo from '@/components/admin-profile-info/AdminProfileInfo';
 import Spinner from '@/components/Spinner';
-import { useOrder } from '@/hooks/admin/order/useOrder';
 import { useUser } from '@/hooks/admin/user/useUser';
 import { Param } from '@/lib/param';
 
 export default function UserPage(props: Param) {
   const { id } = props.params;
   const { data: user, isLoading, refreshUser } = useUser(id);
-  const { data: orders, isLoading: orderLoading } = useOrder(id);
 
-  // make this to a skeleton once the design is ready
-  if (isLoading || orderLoading) {
+  if (isLoading) {
     return (
       <Spinner size='large'>
         <span className='text-xl'>Loading...</span>
