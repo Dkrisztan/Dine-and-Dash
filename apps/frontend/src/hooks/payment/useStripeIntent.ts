5dd714e087ca6ebfe7a2400ef0dc0f1f01f372d2
Dkrisztan <krisztidancs@gmail.com>
2024-12-05 19:25:47 +0100

    remove

--- apps/frontend/src/hooks/payment/useStripeIntent.ts
+++ apps/frontend/src/hooks/payment/useStripeIntent.ts
@@ -1,5 +1,4 @@
 import { useStripe } from '@stripe/react-stripe-js';
-import { PaymentIntent } from '@stripe/stripe-js';
 import { useEffect, useState } from 'react';
 
 export function useStripeIntent(clientSecret: string) {
@@ -9,18 +8,8 @@ export function useStripeIntent(clientSecret: string) {
     if (!stripe) return;
 
     stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
-      setMessage(paymentIntent?.status ? StatusMessages[paymentIntent.status] : undefined);
+      setMessage(paymentIntent?.status ? '' : undefined);
     });
   }, [stripe]);
   return { message };
 }
-
-const StatusMessages: Record<PaymentIntent['status'], string> = {
-  succeeded: 'Fizetés sikeres!',
-  processing: 'Fizetés folyamatban...',
-  requires_payment_method: 'A fizetés nem sikerült, próbáld meg újra.',
-  canceled: 'A fizetés megszakadt.',
-  requires_action: 'A fizetés nem sikerült, próbáld meg újra.',
-  requires_confirmation: 'A fizetés nem sikerült, próbáld meg újra.',
-  requires_capture: 'A fizetés nem sikerült, próbáld meg újra.',
-};
